bed1e37c644983e6be7798179038a885b903a487
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-06-02 09:28:50 -0400

    save

--- 1/2_adder/code.c
+++ 1/2_adder/code.c
@@ -15,15 +15,15 @@ int voltageCounterA = -1;
 int VOLTAGE[] = {0, 5};
 
 void setup() {
-  pinMode(PIN_12, OUTPUT);
-  pinMode(PIN_10, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
+    pinMode(PIN_12, OUTPUT);
+    pinMode(PIN_10, OUTPUT);
+    Serial.begin(9600); // Starts the serial communication
   
-  // welcome text
-  Serial.println("Welcome user!! This program is a 1/2 adder.");
+    // welcome text
+    Serial.println("Welcome user!! This program is a 1/2 adder.");
   
-  // pauses for 5 second
-  delay(PAUSE_TIME_FOR_SETUP);
+    // pauses for 5 second
+    delay(PAUSE_TIME_FOR_SETUP);
 }
 
 void loop() {
