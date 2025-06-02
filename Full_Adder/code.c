01ff972bd0eea66b2357cfdfa1fdb7b061d95454
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-06-02 09:32:08 -0400

    save

--- Full_Adder/code.c
+++ Full_Adder/code.c
@@ -6,7 +6,7 @@
 */
 
 // Variables & Constants
-const int PAUSE_TIME = 1000;
+const int PAUSE_TIME = 2000;
 const int PAUSE_TIME_FOR_SETUP = 5000;
 const int PIN_12 = 12;
 const int PIN_10 = 10;
@@ -16,19 +16,17 @@ int voltageCounterA = -1;
 int voltageCounterCin = -1;
 int VOLTAGE[] = {0, 5};
 
-
-
 void setup() {
-  pinMode(PIN_12, OUTPUT);
-  pinMode(PIN_10, OUTPUT);
-  pinMode(PIN_9, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
+    pinMode(PIN_12, OUTPUT);
+    pinMode(PIN_10, OUTPUT);
+    pinMode(PIN_9, OUTPUT);
+    Serial.begin(9600); // Starts the serial communication
   
-  // welcome text
-  Serial.println("Welcome user!! This program is a full adder.");
+    // welcome text
+    Serial.println("Welcome user!! This program is a full adder.");
   
-  // pauses for 5 second
-  delay(PAUSE_TIME_FOR_SETUP);
+    // pauses for 5 second
+    delay(PAUSE_TIME_FOR_SETUP);
 }
 
 void loop() {
@@ -40,6 +38,13 @@ void loop() {
 
             for (int voltageCounterA = 0; voltageCounterA < 2; voltageCounterA++) {
                 digitalWrite(PIN_10, VOLTAGE[voltageCounterA]);
+
+                Serial.print(voltageCounterA);
+                Serial.print(" + ");
+                Serial.print(voltageCounterB);
+                Serial.print(" + ")
+                Serial.print(voltageCounterCin)
+                Serial.println("\n");
                 
                 delay(PAUSE_TIME);
                 
